a6667f9e1adf09e8a1bbe2af6a7818f78c39de24
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-08-14 01:03:23 -0300

    Finalizando backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const server = express();
 
-// mongoose.connect('mongodb+srv://omnistack:omnistack123456@cluster0-9ofbv.mongodb.net/test?retryWrites=true&w=majority', {
-   // useNewUrlParser: true
-//});
+ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-9ofbv.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
