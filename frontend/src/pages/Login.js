8b082c1bb5e72e51b2cdcffb859fc0906c7e4fee
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-08-17 16:47:01 -0300

    FrontEnd Parte 1

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import logo from '../assets/logo.svg';
+import './Login.css';
+
+export default function Login( { history }) {
+    const [username, setUsername ] = useState('');
+
+    function handleSubmit (e) {
+        e.preventDefault();
+
+        console.log(username);
+
+        history.push('/main');
+    }
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+                <img src={logo} alt="Tindev" />
+                <input
+                    placeholder="Digite seu usuário no GitHub"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                ></input>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <div >
+            <h1>Hello World</h1>
+        
+        </div>
+    );
+}
